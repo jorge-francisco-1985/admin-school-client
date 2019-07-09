b08c60a81314d0c2b0864a3b67fd6cebd1c6b8ad
jorge-francisco-1985 <jorge.francisco@solfine.mx>
2019-07-09 10:18:02 -0500

    agregado diseño de tarjeta a las tabs

--- src/views/login.vue
+++ src/views/login.vue
@@ -1,49 +1,61 @@
 <template>
   <div>
-    <v-toolbar
-      color="cyan"
-      dark
-      tabs
-    >
-      
-      <template v-slot:extension>
-        <v-tabs
-          v-model="model"
-          centered
-          color="cyan"
-          slider-color="yellow"
-        >
-          <v-tab
-            href="#tab-1"
-          >
-            Item 1
-          </v-tab>
-          <v-tab
-            href="#tab-2"
-          >
-            Item 2
-          </v-tab>
-        </v-tabs>
-      </template>
-    </v-toolbar>
+    <v-layout align-center justify-center>
+      <v-flex xs12 sm8 md4>
 
-    <v-tabs-items v-model="model">
-      <v-tab-item
-        value="tab-1"
-      >
-        <v-card flat>
-          <v-card-text v-text="text"></v-card-text>
-        </v-card>
-      </v-tab-item>
 
-      <v-tab-item
-        value="tab-2"
-      >
-        <v-card flat>
-          <v-card-text v-text="text"></v-card-text>
+
+        <v-card class="elevation-12">
+          
+          <v-toolbar
+            color="cyan"
+            dark
+            tabs
+          >            
+            <template v-slot:extension>
+              <v-tabs
+                v-model="model"
+                centered
+                color="cyan"
+                slider-color="yellow"
+              >
+                <v-tab
+                  href="#tab-1"
+                >
+                  Item 1
+                </v-tab>
+                <v-tab
+                  href="#tab-2"
+                >
+                  Item 2
+                </v-tab>
+              </v-tabs>
+            </template>
+          </v-toolbar>
+
+          <v-tabs-items v-model="model">
+            <v-tab-item
+              value="tab-1"
+            >
+              <v-card flat>
+                <v-card-text v-text="text"></v-card-text>
+              </v-card>
+            </v-tab-item>
+
+            <v-tab-item
+              value="tab-2"
+            >
+              <v-card flat>
+                <v-card-text v-text="text"></v-card-text>
+              </v-card>
+            </v-tab-item>
+          </v-tabs-items>
+          <v-card-actions>
+            <v-btn color="primary" type="submit" form="login-form">Login</v-btn>
+          </v-card-actions>
         </v-card>
-      </v-tab-item>
-    </v-tabs-items>
+      </v-flex>
+    </v-layout>    
   </div>
 </template>
 <script>
